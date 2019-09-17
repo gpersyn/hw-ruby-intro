9097db012e1e8b7754a2b660707a57492cd309f3
EC2 Default User <ec2-user@ip-172-31-46-220.us-east-2.compute.internal>
2019-09-17 16:47:32 +0000

    Finished Hw-part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,16 +3,41 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  a = 0
+  arr.each{ |i| a += i} #adds all values in array
+  return a
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum arr #sums 2 largest values in array
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    sum = 0
+    sum = arr.max
+    arr.delete_at(arr.index(arr.max)) #deletes max value from array
+    sum += arr.max
+    return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  end
+  
+  for i in 0..arr.length-2 do
+    for j in arr.drop(i+1) do
+      sum = arr.fetch(i) + j
+      if sum == n.to_i 
+        return true
+      end
+    end
+  end
+  return false
 end
+
 
 # Part 2
 
